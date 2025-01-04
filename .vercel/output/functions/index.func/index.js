ce72bfff13ca6e4aa124f8840e549376f3f11553
chauncey <chauncey.philpot@proton.me>
2025-01-04 18:29:00 -0500

    working

---
+++ .vercel/output/functions/index.func/index.js
@@ -0,0 +1,18 @@
+import { b } from "./assets/app-BQPVLIAH.js";
+import "fs";
+import "path";
+import "stream";
+import "tty";
+import "util";
+import "net";
+import "events";
+import "zlib";
+import "buffer";
+import "string_decoder";
+import "querystring";
+import "url";
+import "http";
+import "crypto";
+export {
+  b as default
+};
---
+++ app/routes/home.tsx
@@ -0,0 +1,23 @@
+import type { Route } from "./+types/home";
+import { Welcome } from "../welcome/welcome";
+import { PrismaClient } from "@prisma/client";
+
+export function meta({}: Route.MetaArgs) {
+  return [
+    { title: "New React Router App" },
+    { name: "description", content: "Welcome to React Router!" },
+  ];
+}
+
+export async function loader({ context }: Route.LoaderArgs) {
+  const prisma = new PrismaClient();
+  const users = await prisma.user.findFirst();
+  return { message: context.VALUE_FROM_VERCEL, users };
+}
+
+export default function Home({ loaderData }: Route.ComponentProps) {
+  return <>
+  <Welcome message={loaderData.message} />
+  <div>{loaderData.users?.name}</div>
+  </>;
+}
